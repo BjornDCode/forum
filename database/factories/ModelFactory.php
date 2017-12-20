c0af87038a017cc391112ad9601241a00f9f6155
Bjorn Lindholm <bjornlindholmhansen@gmail.com>
2017-12-21 00:37:48 +0100

    Thread + reply models

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Thread;
+
 /*
 |--------------------------------------------------------------------------
 | Model Factories
@@ -20,5 +22,27 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'email' => $faker->unique()->safeEmail,
         'password' => $password ?: $password = bcrypt('secret'),
         'remember_token' => str_random(10),
+    ];
+});
+
+$factory->define(App\Thread::class, function ($faker) {
+    return [
+        'user_id' => function() {
+            return factory('App\User')->create()->id;
+        },
+        'title' => $faker->sentence,
+        'body' => $faker->paragraph
+    ];
+});
+
+$factory->define(App\Reply::class, function ($faker) {
+    return [
+        'thread_id' => function() {
+            return factory('App\Thread')->create()->id;
+        },   
+        'user_id' => function() {
+            return factory('App\User')->create()->id;
+        },
+        'body' => $faker->paragraph
     ];
 });
