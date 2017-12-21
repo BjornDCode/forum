028e7b818efaab17f78d585d12c482cbf593367f
Bjorn Lindholm <bjornlindholmhansen@gmail.com>
2017-12-21 15:02:54 +0100

    Validation

--- tests/Feature/ParticipateInForumTest.php
+++ tests/Feature/ParticipateInForumTest.php
@@ -32,4 +32,17 @@ class ParticipateInForumTest extends TestCase
              ->assertSee($reply->body);
     }
 
+    /** @test */
+    public function a_reply_required_a_body()
+    {
+        $this->withExceptionHandling()->signIn();
+
+        $thread = create('App\Thread');
+        $reply = make('App\Reply', ['body' => null]);
+
+        $this->post($thread->path() . '/replies', $reply->toArray())
+             ->assertSessionHasErrors('body');
+
+    }
+
 }
