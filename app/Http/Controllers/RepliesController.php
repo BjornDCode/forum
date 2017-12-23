826201229c0a14af203682a401ad79d71b837d6c
Bjorn Lindholm <bjornlindholmhansen@gmail.com>
2017-12-23 17:19:01 +0100

    Delete replies

--- app/Http/Controllers/RepliesController.php
+++ app/Http/Controllers/RepliesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Reply;
 use App\Thread;
 use Illuminate\Http\Request;
 
@@ -26,6 +27,15 @@ class RepliesController extends Controller
         ]);
 
         return back()->with('flash', 'Your reply has been saved');
+    }
+
+    public function destroy(Reply $reply) 
+    {
+        $this->authorize('update', $reply);
+
+        $reply->delete();
+
+        return back();
     }
 
 }
