e1e41b4e877f3c4f164a457f46b8b4af2daee4bc
Bjorn Lindholm <bjornlindholmhansen@gmail.com>
2017-12-26 00:12:43 +0100

    Notifications component

---
+++ resources/assets/js/components/UserNotifications.vue
@@ -0,0 +1,34 @@
+<template>
+    <li class="dropdown" >
+        <a href="$" class="dropdown-toggle" data-toggle="dropdown">
+            <span class="glyphicon glyphicon-bell"></span>
+        </a>
+
+        <ul class="dropdown-menu" v-if="notifications.length">
+            <li v-for="notification in notifications">
+                <a :href="notification.data.link" v-text="notification.data.message" @click="markAsRead(notification)"></a>
+            </li>
+        </ul>
+    </li>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                notifications: false
+            }
+        },
+
+        created() {
+            axios.get('/profiles/' + window.App.user.name + '/notifications')
+                 .then(response => this.notifications = response.data);
+        },
+
+        methods: {
+            markAsRead(notification) {
+                axios.delete('/profiles/' + window.App.user.name + '/notifications/' + notification.id);
+            }
+        }
+    }
+</script>
