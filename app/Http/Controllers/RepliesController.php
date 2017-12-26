c6473b886e021676284afe87fa33bbc88d608a3d
Bjorn Lindholm <bjornlindholmhansen@gmail.com>
2017-12-26 15:29:49 +0100

    Spamfree validation rule

--- app/Http/Controllers/RepliesController.php
+++ app/Http/Controllers/RepliesController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Inspections\Spam;
 use App\Reply;
 use App\Thread;
 use Illuminate\Http\Request;
@@ -18,7 +17,7 @@ class RepliesController extends Controller
     public function store($channelId, Thread $thread) 
     {
         try {
-            $this->validateReply();
+            $this->validate(request(), ['body' => 'required|spamfree']);
 
             $reply =  $thread->addReply([
             'body' => request('body'),
@@ -49,7 +48,7 @@ class RepliesController extends Controller
         $this->authorize('update', $reply);
 
         try {
-            $this->validateReply();
+            $this->validate(request(), ['body' => 'required|spamfree']);
 
             $reply->update(['body' => request('body')]);
         } catch (\Exception $e) {
@@ -62,12 +61,6 @@ class RepliesController extends Controller
     public function index($channelId, Thread $thread) 
     {
         return $thread->replies()->paginate(10);
-    }
-
-    protected function validateReply() 
-    {
-        $this->validate(request(), ['body' => 'required']);
-        resolve(Spam::class)->detect(request('body'));
     }
 
 }
