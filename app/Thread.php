99aaf18442816e0b198fabc54910a1d7f3d44098
Bjorn Lindholm <bjornlindholmhansen@gmail.com>
2017-12-26 11:27:18 +0100

    Updated thread notification

--- app/Thread.php
+++ app/Thread.php
@@ -92,4 +92,11 @@ class Thread extends Model
                     ->exists();
     }
 
+    public function hasUpdatesFor($user) 
+    {
+        $key = $user->visitedThreadCacheKey($this);
+ 
+        return $this->updated_at > cache($key);
+    }
+
 }
